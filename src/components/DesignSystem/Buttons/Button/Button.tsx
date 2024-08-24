6d0869ff7dddab6ef203c6f62c23352854006f8f
Matt <matthieu.coisnon@decathlon.com>
2024-08-25 01:48:31 +0200

    feat(design): create generic button

--- src/components/DesignSystem/Buttons/Button/Button.tsx
+++ src/components/DesignSystem/Buttons/Button/Button.tsx
@@ -5,14 +5,16 @@ import './Button.scss';
 interface ButtonProps {
   label?: string;
   iconComponent?: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, iconComponent }) => {
+const Button: React.FC<ButtonProps> = ({ label, iconComponent, disabled, className }) => {
   return (
     <FlexBox>
-      <button className="gustave-button">
+      <button className={`gustave-button ${className} ${disabled ? 'disabled' : ''}`}>
         {iconComponent}
-        <span>{label}</span>
+        {label}
       </button>
     </FlexBox>
   );
